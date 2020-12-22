dd136e9d660614da25e9239fb6882ea9b033373b
Henkou <fjksr1@gmail.com>
2020-12-22 12:04:42 +0900

    Getting Weather part Two API But..

--- weather.js
+++ weather.js
@@ -1,10 +1,18 @@
+const API_KEY = "0a1933890d59f7db7a56ad54acbf8a78";
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+  );
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
 function handleGeoSuccess(position) {
+  console.log(position);
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
   const coordsObj = {
@@ -14,27 +22,31 @@ function handleGeoSuccess(position) {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
   console.log("Cant access geo location");
 }
 
-function askForCoords() {
-  navigator.geolocation.watchPosition(handleGeoSuccess, handleGeoError);
-}
 // function askForCoords() {
-//   navigator.geolocation.watchPosition((position) => {
-//     doSomething(position.coords.latitude, position.coords.longitude);
-//   });
+//   navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
 // }
+function askForCoords() {
+  navigator.geolocation.watchPosition((position) => {
+    doSomething(position.coords.latitude, position.coords.longitude);
+  });
+}
 
 function loadCoords() {
-  const loadedCords = localStorage.getItem(COORDS);
-  if (loadedCords === null) {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
     askForCoords();
   } else {
     // getWeather
+    const parseCoords = JSON.parse(loadedCoords);
+    console.log(parseCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 
